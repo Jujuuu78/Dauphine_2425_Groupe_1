6a625f3b70f1f2548d88188a02f765433a2f7307
Jujuuu78 <julienrameaux@MacBook-Pro-de-Julien.local>
2025-03-04 16:51:45 +0100

    Ajout de la page membres + problématique et sujet

--- web_app/Home.py
+++ web_app/Home.py
@@ -5,46 +5,11 @@
 import streamlit as st
 
 # Page Title
-st.title("Welcome to the Application Development Project")
+st.title("Processus de transactions bancaires")
 
 # Introduction
 st.markdown("""
-Welcome to this application development course at Dauphine.
-The goal of this project is to enable each group to develop their own application using a series of modern and powerful technologies.
+**Problématique :** 
 
-## Technologies Used
-For this project, you will work with the following technologies:
-
-1. **Streamlit**: To create interactive and user-friendly web interfaces. Streamlit simplifies the process of building custom web applications with Python.
-
-2. **Cohere**: A natural language processing (NLP) model that offers powerful capabilities for text analysis, text generation, and other NLP tasks.
-
-3. **FastAPI**: A modern and fast web framework for building APIs with Python. It is ideal for creating performant and scalable backend services.
-
-## Project Objectives
-Each group is expected to create a functional application using the technologies mentioned above. The application should address a specific need and be accessible via a web interface.
-
-## Project Structure
-1. **Phase 1: Design** - Define the problem you want to solve with your application and design a solution using the mentioned technologies.
-
-2. **Phase 2: Development** - Develop the application using Streamlit for the interface, Cohere for NLP processing and FastAPI for the backend.
-
-3. **Phase 3: Deployment** - Deploy your application on a platform of your choice, ensuring it is accessible and functional.
-
-## Resources and Support
-We will provide tutorials and documentation to help you familiarize yourself with these technologies. Technical support will also be available to answer your questions throughout the project.
-
-## Evaluation Criteria
-Your application will be evaluated based on the following criteria
-- Relevance and originality of the proposed solution.
-- Effective or simulated integration of various technologies.
-- The quality of the user interface.
-- Robustness and design of the system architecture
-
-Good luck to everyone, and may the best project win!
-
-
-
-## Now, you can either delete this text (or save it in a new tab) and describe your problem instead
-
+Comment améliorer la performance et la sécurité des transactions bancaires (virements, prélèvements) ?
 """)
---
+++ web_app/pages/3_Members.py
@@ -0,0 +1,17 @@
+# Page Title
+import streamlit as st
+
+st.title("Group members")
+
+# Introduction
+st.markdown(
+    """
+    Ilyess BATAL
+    
+    Cyril RAMEAUX
+    
+    Julien RAMEAUX
+    
+    Arvinde SENGUTTUVAN
+    """
+)
